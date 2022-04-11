5dbc4391e80d50d9e0cafb59bc1c46e92b6090b9
prolight14 <evan.sweet@outlook.com>
2022-04-10 18:05:16 -0600

    Removed unneeded things

--- nodeSrc/scenes/TitleScene.js
+++ nodeSrc/scenes/TitleScene.js
@@ -19,14 +19,13 @@ var TitleScene = (function (_super) {
         return _super.call(this, "title") || this;
     }
     TitleScene.prototype.preload = function () {
-        this.load.image("planetSearch", "./assets/Title/PlanetSearch.png");
-        this.load.bitmapFont("PixelFont", "./assets/Fonts/PixelFont.png", "./assets/Fonts/PixelFont.xml");
+        this.load.image("planetSearch3", "./assets/Title/PlanetSearch3.png");
     };
     TitleScene.prototype.create = function () {
         var _this = this;
         var gameWidth = this.game.canvas.width;
         var gameHeight = this.game.canvas.height;
-        this.add.image(0, 0, "planetSearch").setOrigin(0, 0).setDisplaySize(gameWidth, gameHeight);
+        this.add.image(0, 0, "planetSearch3").setOrigin(0, 0).setDisplaySize(gameWidth, gameHeight);
         this.add.text(gameWidth * 0.5, gameHeight * 0.7, "Press any key to play!").setOrigin(0.5).setAlign("center");
         this.input.keyboard.once("keydown", function () {
             _this.cameras.main.fadeOut(500, 0, 0, 0);
@@ -34,11 +33,6 @@ var TitleScene = (function (_super) {
                 _this.scene.start("entry");
             });
         });
-        this.add.bitmapText(400, 260, "PixelFont", "ABC|D|E").setScale(4);
-    };
-    TitleScene.prototype.loadBitmapText = function () {
-    };
-    TitleScene.prototype.drawBitmapText = function () {
     };
     return TitleScene;
 }(Phaser.Scene));
